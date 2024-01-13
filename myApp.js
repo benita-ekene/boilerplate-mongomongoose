1ec4198278f79cf93d7f336cfdc771edf90585d3
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2024-01-13 17:00:13 +0100

    connected to mongoDB Atlas

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://benny:Ojugoo@3644@cluster0.fdyjiub.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// // mongoose.connect(process.env.MONGO_URI);
 
 
 let Person;
