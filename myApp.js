aa7a210b5b07aabc5f69f76bd43f7953ba73ac3f
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2024-01-14 11:59:50 +0100

    created Person Model

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const personSchema = new mongoose.Schema({
+const personSchema = new Schema({
   name: {
     type: String,
     required: true
@@ -14,7 +14,7 @@ const personSchema = new mongoose.Schema({
     
 });
 
-let Person = mongoose.model('Person', personSchema);
+let Person = model('Person', personSchema);
 
 // let Person;
 
